d1644e1c8dd3e0e4503b9e32c65a14a10fbcc4dd
juicyluv <juicyluv@mail.ru>
2021-12-31 23:11:20 +0300

    the last commit in this year... testing validator

--- internal/handler/track.go
+++ internal/handler/track.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/juicyluv/advanced-rest-server/internal/model"
+	"github.com/juicyluv/advanced-rest-server/internal/validator"
 )
 
 func (h *Handler) getTrack(w http.ResponseWriter, r *http.Request) {
@@ -34,10 +35,26 @@ func (h *Handler) getTrack(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) createTrack(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Title string `json:"title"`
+		Year  int32  `json:"year"`
 	}
 
 	if err := readJSON(w, r, &input); err != nil {
 		badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "must not be empty")
+	v.Check(len(input.Title) >= 1, "title", "the length must be at least 1 character")
+	v.Check(len(input.Title) <= 50, "title", "the length must be not greater than 50 characters")
+
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year > 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must be not greater than current year")
+
+	if !v.Valid() {
+		failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
