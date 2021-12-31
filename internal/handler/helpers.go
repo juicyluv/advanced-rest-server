ec1c5686278976c8b8012244e4dc2b288d610e88
= <=>
2021-12-31 16:08:52 +0300

    moved health() function to info.go, added comments to all functions

--- internal/handler/helpers.go
+++ internal/handler/helpers.go
@@ -10,21 +10,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (h *Handler) health(w http.ResponseWriter, r *http.Request) {
-	obj := map[string]interface{}{
-		"status":  "available",
-		"version": "1.0.0",
-		"build":   "development",
-	}
-
-	if err := sendJSON(w, &obj, http.StatusOK, nil); err != nil {
-		message := err.Error()
-		log.Println("An error occurred: " + message)
-		http.Error(w, message, http.StatusInternalServerError)
-	}
-}
-
-// sendJSON sends given object in JSON format
+// sendJSON sends given object in JSON format with given headers.
 func sendJSON(w http.ResponseWriter, data interface{}, statusCode int, headers http.Header) error {
 	obj, err := json.Marshal(data)
 	if err != nil {
@@ -42,9 +28,8 @@ func sendJSON(w http.ResponseWriter, data interface{}, statusCode int, headers h
 	return nil
 }
 
-// Returns an "id" url param as int and error if any.
-// Example: "/api/v1/movies/32" returns (32, nil).
-func readIDParam(r *http.Request) (int64, error) {
+// readIdParam returns an id of the URL path on success.
+func readIdParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
@@ -53,6 +38,7 @@ func readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
+// logError logs an error to logger output.
 func logError(r *http.Request, err error) {
 	log.Println(err)
 }
