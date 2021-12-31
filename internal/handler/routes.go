76fea4b66d33d95211b30e52f5c5f376a9891bd9
juicyluv <juicyluv@mail.ru>
2021-12-31 19:24:29 +0300

    now this file will include all the api urls and its handlers

---
+++ internal/handler/routes.go
@@ -0,0 +1,13 @@
+package handler
+
+import "net/http"
+
+// initRoutes initializes handlers for API routes.
+func initRoutes(h *Handler) {
+	h.router.NotFound = http.HandlerFunc(notFoundResponse)
+	h.router.MethodNotAllowed = http.HandlerFunc(methodNotAllowedResponse)
+
+	h.router.HandlerFunc(http.MethodGet, "/api/v1/health", h.health)
+	h.router.HandlerFunc(http.MethodGet, "/api/v1/tracks/:id", h.getTrack)
+	h.router.HandlerFunc(http.MethodPost, "/api/v1/tracks", h.createTrack)
+}
