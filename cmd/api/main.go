288de0fb118ebdbba7f1ee5d587bc2b03cbbbeed
juicyluv <juicyluv@mail.ru>
2022-01-01 22:50:26 +0300

    added config flags

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	"github.com/juicyluv/advanced-rest-server/config"
 	"github.com/juicyluv/advanced-rest-server/internal/server"
 )
 
+var (
+	configPath = flag.String("config-path", "config", "config directory path")
+	configName = flag.String("config-name", "server", "config filename without extension")
+)
+
 func main() {
+	flag.Parse()
+
+	if err := config.LoadConfigs(*configPath, *configName); err != nil {
+		log.Fatalln("An error occurred while loading config file. Please, check config path and filename.")
+	}
+
 	config := server.NewConfig()
 	server := server.New(config)
 
