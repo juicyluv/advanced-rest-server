99f4577542c0e91a24a52681ad01a31bae37df6e
juicyluv <juicyluv@mail.ru>
2022-01-01 20:41:41 +0300

    added Validate function to the track struct

--- internal/model/track.go
+++ internal/model/track.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/juicyluv/advanced-rest-server/internal/validator"
+)
 
 type Track struct {
 	Id        int64     `json:"id"`
@@ -10,5 +14,28 @@ type Track struct {
 	Year      int32     `json:"year,omitempty"`
 	Duration  int32     `json:"duration"`
 	Genres    []string  `json:"genres,omitempty"`
-	Artists   []string  `json:"artists,omitempty"`
+	Artists   []string  `json:"artists"`
+	Album     string    `json:"album,omitempty"`
+}
+
+// Validate checks whether track instance is valid. If it's not, it
+// will fill up given validator Error map with appropriate error messages.
+func (t *Track) Validate(v *validator.Validator) {
+	// Title
+	v.NotEmpty(t.Title, "title")
+	v.MaxLength(t.Title, 100, "title")
+
+	// Year
+	v.Min(int(t.Year), 1888, "year")
+	v.Max(int(t.Year), time.Now().Year(), "year")
+
+	// Duration
+	v.Min(int(t.Duration), 1, "duration")
+
+	// Genres, Artists
+	v.UniqueStrings(t.Genres)
+	v.UniqueStrings(t.Artists)
+
+	// Album
+	v.NotEmpty(t.Album, "album")
 }
