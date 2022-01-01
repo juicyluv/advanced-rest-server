5a151c6f9ce6b8cd5ee0c72071a1d7aa150cc4e8
juicyluv <juicyluv@mail.ru>
2022-01-01 23:43:28 +0300

    added database config and proper config initialization

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,15 +1,39 @@
 package server
 
-import "github.com/juicyluv/advanced-rest-server/internal/logger"
+import (
+	"time"
+
+	"github.com/juicyluv/advanced-rest-server/internal/logger"
+	"github.com/spf13/viper"
+)
+
+type db struct {
+	Username string
+	DbName   string
+	Port     string
+	Host     string
+	SSLMode  string
+}
 
 type config struct {
-	Port     string
-	LogLevel int
+	Db             db
+	Port           string
+	ReadTimeout    time.Duration
+	WriteTimeout   time.Duration
+	LogLevel       logger.LogLevel
+	MaxHeaderBytes int
 }
 
 func NewConfig() *config {
 	return &config{
-		Port:     "3000",
+		Db: db{
+			Port:     viper.GetString("db.postgres.port"),
+			Username: viper.GetString("db.postgres.username"),
+			DbName:   viper.GetString("db.postgres.dbname"),
+			Host:     viper.GetString("db.postgres.host"),
+			SSLMode:  viper.GetString("db.postgres.sslmode"),
+		},
+		Port:     viper.GetString("http.port"),
 		LogLevel: logger.LevelDebug,
 	}
 }
