316f2fa3060bb10611ed0cf51669f76d7d2db43b
juicyluv <juicyluv@mail.ru>
2022-01-03 04:53:32 +0300

    updated create track

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,21 +1,25 @@
 package handler
 
 import (
+	"github.com/juicyluv/advanced-rest-server/internal/repository"
+	"github.com/juicyluv/advanced-rest-server/internal/service"
 	"github.com/julienschmidt/httprouter"
 )
 
 // Handler is used as http.Server handler for handling HTTP requests.
 type Handler struct {
-	router *httprouter.Router
+	router  *httprouter.Router
+	service *service.Service
 }
 
 // jsonResponse is used to send response with the data in JSON format.
 type jsonResponse map[string]interface{}
 
 // New returns a new Handler instance.
-func New() *Handler {
+func New(repository *repository.Repository) *Handler {
 	handler := &Handler{
-		router: httprouter.New(),
+		router:  httprouter.New(),
+		service: service.NewService(repository),
 	}
 
 	initRoutes(handler)
