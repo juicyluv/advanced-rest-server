4d6f90057a8fb66477a0c16d03c5fe7c6c1d96c0
juicyluv <juicyluv@mail.ru>
2022-01-03 04:55:14 +0300

    now server uses db instance to create repository

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,25 +3,30 @@ package server
 import (
 	"net/http"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/juicyluv/advanced-rest-server/internal/handler"
 	"github.com/juicyluv/advanced-rest-server/internal/logger"
+	"github.com/juicyluv/advanced-rest-server/internal/repository"
 )
 
 type server struct {
 	server *http.Server
 	config *Config
 	logger logger.Logging
+	db     *sqlx.DB
 }
 
-func New(cfg *Config, logger logger.Logging) *server {
+func New(cfg *Config, logger logger.Logging, db *sqlx.DB) *server {
 	return &server{
 		config: cfg,
 		logger: logger,
+		db:     db,
 	}
 }
 
 func (s *server) Run() error {
-	router := handler.New()
+	r := repository.NewRepository(s.db)
+	router := handler.New(r)
 
 	s.server = &http.Server{
 		Addr:    ":" + s.config.Port,
