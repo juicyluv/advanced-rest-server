9a4c63bccec976456ace8feeb969ec0d161bde87
juicyluv <juicyluv@mail.ru>
2022-01-04 16:57:09 +0300

    added delete cascade

--- migrations/000004_create_track_genre_table.up.sql
+++ migrations/000004_create_track_genre_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS track_genre (
     track_id BIGINT NOT NULL,
     genre_id SMALLINT NOT NULL,
 
-    FOREIGN KEY(track_id) REFERENCES track(track_id),
-    FOREIGN KEY(genre_id) REFERENCES genre(genre_id)
+    FOREIGN KEY(track_id) REFERENCES track(track_id) ON DELETE CASCADE,
+    FOREIGN KEY(genre_id) REFERENCES genre(genre_id) ON DELETE CASCADE
 );
