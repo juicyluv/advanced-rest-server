8988f7155742c61c57be4b3603a4b0b66698c636
ellywynn <canttrymybest@gmail.com>
2022-01-10 20:47:31 +0300

    added a route for retrieving all tracks

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -8,7 +8,9 @@ func initRoutes(h *Handler) {
 	h.router.MethodNotAllowed = http.HandlerFunc(methodNotAllowedResponse)
 
 	h.router.HandlerFunc(http.MethodGet, "/api/v1/health", h.health)
+
 	h.router.HandlerFunc(http.MethodGet, "/api/v1/tracks/:id", h.getTrack)
+	h.router.HandlerFunc(http.MethodGet, "/api/v1/tracks", h.getListOfTracks)
 	h.router.HandlerFunc(http.MethodPost, "/api/v1/tracks", h.createTrack)
 	h.router.HandlerFunc(http.MethodPut, "/api/v1/tracks/:id", h.updateTrack)
 	h.router.HandlerFunc(http.MethodDelete, "/api/v1/tracks/:id", h.deleteTrack)
